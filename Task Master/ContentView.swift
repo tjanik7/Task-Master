e0067540592d5af1cf7048db3b75d9f290101544
Ty Janik <57628960+tjanik7@users.noreply.github.com>
2026-06-28 10:32:46 -0400

    Task list now has sections by date

--- Task Master/ContentView.swift
+++ Task Master/ContentView.swift
@@ -11,18 +11,86 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @State private var path = [String]()
-    
+
     //    @Query private var tasks: [Task] // TODO: Implement storage
-    
+
     // Mocked data
     @State private var tasks: [Task] = [
         Task(name: "Dishes", dueDate: Date.now),
-        Task(name: "Laundry", dueDate: Calendar.current.date(byAdding: .day, value: 7, to: Date.now)!),
-        Task(name: "Vacuum", dueDate: Calendar.current.date(byAdding: .day, value: 2, to: Date.now)!)
+        Task(
+            name: "Laundry",
+            dueDate: Calendar.current.date(
+                byAdding: .day,
+                value: 7,
+                to: Date.now
+            )!
+        ),
+        Task(
+            name: "Vacuum",
+            dueDate: Calendar.current.date(
+                byAdding: .day,
+                value: 2,
+                to: Date.now
+            )!
+        ),
+        Task(
+            name: "Clean the fridge",
+            dueDate: Calendar.current.date(
+                byAdding: .day,
+                value: 31,
+                to: Date.now
+            )!
+        )
     ]
-    
+
     private var sortedTasks: [Task] {
         tasks.sorted { $0.dueDate < $1.dueDate }
+    }
+    
+    private func isToday(d: Date) -> Bool {
+        Calendar.current.isDate(d, inSameDayAs: Date.now)
+    }
+
+    private var tasksDueToday: [Task] {
+        tasks.filter { task in
+            isToday(d: task.dueDate)
+        }
+    }
+    
+    private func isThisWeek(earlier: Date, later: Date) -> Bool {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.day], from: earlier, to: later)
+        
+        if let days = components.day {
+            return days <= 7
+        }
+        
+        return false
+    }
+    
+    // Tasks with day delta > 1 and <= 7
+    private var tasksDueThisWeek: [Task] {
+        tasks.filter { task in
+            isThisWeek(earlier: Date.now, later: task.dueDate) && !isToday(d: task.dueDate)
+        }
+    }
+    
+    // Tasks with day delta > 7
+    private func isLater(d: Date) -> Bool {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.day], from: Date.now, to: d)
+        
+        if let days = components.day {
+            return days > 7
+        }
+        
+        return false
+    }
+    
+    private var tasksDueLater: [Task] {
+        tasks.filter { task in
+            isLater(d: task.dueDate)
+        }
     }
 
     @State private var showForm = false
@@ -34,19 +102,62 @@ struct ContentView: View {
                     showForm = true
                 }
             }
-            List(sortedTasks) { task in
+            
+            Text("Due Today")
+            List(tasksDueToday) { task in
                 NavigationLink(value: task) {
                     HStack {
                         Text(task.name)
                             .font(.headline)
-                        
+
                         Spacer()
-                        
+
                         Text(task.dueDate.formatted())
                             .font(.subheadline)
                             .foregroundColor(.secondary)
                     }
                 }
+            }
+            .navigationDestination(for: Task.self) { selectedTask in
+                TaskFormView(task: selectedTask, onUpdate: updateTask)
+            }
+            
+            Text("Due This Week")
+            List(tasksDueThisWeek) { task in
+                NavigationLink(value: task) {
+                    HStack {
+                        Text(task.name)
+                            .font(.headline)
+
+                        Spacer()
+
+                        Text(task.dueDate.formatted())
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationDestination(for: Task.self) { selectedTask in
+                TaskFormView(task: selectedTask, onUpdate: updateTask)
+            }
+            
+            Text("Due Later")
+            List(tasksDueLater) { task in
+                NavigationLink(value: task) {
+                    HStack {
+                        Text(task.name)
+                            .font(.headline)
+
+                        Spacer()
+
+                        Text(task.dueDate.formatted())
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationDestination(for: Task.self) { selectedTask in
+                TaskFormView(task: selectedTask, onUpdate: updateTask)
             }
             .navigationDestination(for: Task.self) { selectedTask in
                 TaskFormView(task: selectedTask, onUpdate: updateTask)
@@ -63,12 +174,12 @@ struct ContentView: View {
             print("Dismissing")
         }
     }
-    
+
     private func createTask(task: Task) {
         tasks.append(task)
         dismissOverlay()
     }
-    
+
     private func updateTask(task: Task) {
         for index in tasks.indices {
             if tasks[index].id == task.id {
@@ -77,7 +188,7 @@ struct ContentView: View {
             }
         }
     }
-    
+
 }
 
 #Preview {
